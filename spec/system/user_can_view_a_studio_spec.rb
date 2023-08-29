fe6a4b96ec34d5f750a211b9454874bd19f0b8e4
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-08-29 09:59:27 -0500

    complete user can view a studio

---
+++ spec/system/user_can_view_a_studio_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Viewing Studios", type: :feature do
+  scenario "I can see a studio in the system" do
+    madhouse = Studio.create(
+      name: "Madhouse",
+      japanese: true,
+      rank: 15,
+      founded_on: "1992-11-17"
+    )
+
+    visit "/studios/#{madhouse.id}"
+
+    save_and_open_page
+
+    expect(page).to have_content "Madhouse"
+    expect(page).to have_content "true"
+    expect(page).to have_content "15"
+    expect(page).to have_content "1992-11-17"
+    #    expect(page).to have_content madhouse.created_at
+    #    expect(page).to have_content madhouse.updated_at
+  end
+end
