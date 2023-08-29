9b0352d60bb6d6a286befa232beed07217c6ad85
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-08-28 21:40:54 -0500

    add viewing studios

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_023016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.boolean "japanese"
+    t.integer "rank"
+    t.date "founded_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
